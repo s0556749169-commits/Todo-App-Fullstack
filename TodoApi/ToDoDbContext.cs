24e260901d0f1b3631526cba5286aef6d5d82368
s0556749169-commit <s0556749169@gmail.com>
2026-04-16 18:27:54 +0300

    Update DbContext to use Render connection string

--- TodoApi/ToDoDbContext.cs
+++ TodoApi/ToDoDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
 
 namespace TodoApi;
 
@@ -15,11 +14,27 @@ public partial class ToDoDbContext : DbContext
         : base(options)
     {
     }
+
     public DbSet<User> Users { get; set; }
     public virtual DbSet<Item> Items { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.45-mysql"));
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            // 1. ניסיון לקרוא את הכתובת מהמשתנה שהגדרת ב-Render
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+            // 2. אם את במחשב המקומי (והמשתנה ריק), השתמשי בכתובת המקומית שלך
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                // כאן את יכולה לשים את מחרוזת החיבור למחשב שלך בבית אם תרצי
+                connectionString = "server=localhost;database=todo;user=root;password=your_password";
+            }
+
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -30,9 +45,7 @@ public partial class ToDoDbContext : DbContext
         modelBuilder.Entity<Item>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.ToTable("items");
-
             entity.Property(e => e.Name).HasMaxLength(100);
         });
 
