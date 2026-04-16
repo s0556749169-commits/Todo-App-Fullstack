aeb9802e65d941e76def3ed4d36065df0456fe4f
s0556749169-commit <s0556749169@gmail.com>
2026-04-16 18:41:06 +0300

    Update DbContext to use Render connection string

--- TodoApi/ToDoDbContext.cs
+++ TodoApi/ToDoDbContext.cs
@@ -1,57 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-
-namespace TodoApi;
-
-public partial class ToDoDbContext : DbContext
-{
-    public ToDoDbContext()
-    {
-    }
-
-    public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
-        : base(options)
-    {
-    }
-
-    public DbSet<User> Users { get; set; }
-    public virtual DbSet<Item> Items { get; set; }
-
- protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+﻿protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 {
     if (!optionsBuilder.IsConfigured)
     {
         var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
-
+        
+        // אם אין משתנה סביבה (כמו במחשב שלך), הוא ישתמש ב-Localhost
         if (string.IsNullOrEmpty(connectionString))
         {
-            // מחרוזת לבדיקה במחשב האישי
             connectionString = "server=localhost;database=todo;user=root;password=your_password";
         }
 
-        // שימוש בגרסה קבועה של MySQL במקום AutoDetect כדי למנוע את השגיאה שראינו ב-Logs
+        // שימוש בגרסה קבועה (8.0.30) מונע מהשרת לקרוס בניסיון לזהות את הדאטהבייס
         var serverVersion = new MySqlServerVersion(new Version(8, 0, 30)); 
-
         optionsBuilder.UseMySql(connectionString, serverVersion);
     }
 }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder
-            .UseCollation("utf8mb4_0900_ai_ci")
-            .HasCharSet("utf8mb4");
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    // חשוב מאוד: הגדרת שמות טבלאות באותיות קטנות (linux compatible)
+    modelBuilder.Entity<User>(entity => {
+        entity.ToTable("users"); 
+        entity.HasKey(e => e.Id);
+    });
 
-        modelBuilder.Entity<Item>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("PRIMARY");
-            entity.ToTable("items");
-            entity.Property(e => e.Name).HasMaxLength(100);
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    modelBuilder.Entity<Item>(entity => {
+        entity.ToTable("items");
+        entity.HasKey(e => e.Id);
+    });
 }
